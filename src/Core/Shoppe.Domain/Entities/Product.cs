2ee50446272677da5a788f2273c4017dad5f490e
nurlancreus <nurlancreus007@gmail.com>
2024-09-09 17:59:40 +0400

    implement entity folder

---
+++ src/Core/Shoppe.Domain/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿using Shoppe.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoppe.Domain.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; } = null!;
+    }
+}
