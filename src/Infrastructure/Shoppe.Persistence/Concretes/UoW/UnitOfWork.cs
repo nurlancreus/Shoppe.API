e18683a21e2e54af56e947186a54aa07706525ed
nurlancreus <nurlancreus007@gmail.com>
2024-09-09 18:01:00 +0400

    configuring db context

---
+++ src/Infrastructure/Shoppe.Persistence/Concretes/UoW/UnitOfWork.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using Shoppe.Application.Abstractions.Repositories;
+using Shoppe.Application.Abstractions.UoW;
+using Shoppe.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoppe.Persistence.Concretes.UoW
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ShoppeDbContext _context;
+        //private IDbContextTransaction _transaction;
+        private bool _disposed = false;
+
+        // Entity-specific repositories
+        public IProductReadRepository ProductReadRepository { get; private set; }
+        public IProductWriteRepository ProductWriteRepository { get; private set; }
+
+        public UnitOfWork(ShoppeDbContext context, IProductReadRepository productReadRepository, IProductWriteRepository productWriteRepository)
+        {
+            _context = context;
+            //_transaction = transaction;
+            ProductReadRepository = productReadRepository;
+            ProductWriteRepository = productWriteRepository;
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task BeginTransactionAsync()
+        {
+
+            using var transaction = await _context.Database.BeginTransactionAsync();
+        }
+
+        public async Task CommitTransactionAsync()
+        {
+            try
+            {
+                await _context.SaveChangesAsync();
+                //if (_transaction != null)
+                //{
+                //    await _transaction.CommitAsync();
+                //}
+            }
+            catch
+            {
+                await RollbackTransactionAsync();
+                throw;
+            }
+            finally
+            {
+                //if (_transaction != null)
+                //{
+                //    await _transaction.DisposeAsync();
+                //    _transaction = null;
+                //}
+            }
+        }
+
+        public async Task RollbackTransactionAsync()
+        {
+            //if (_transaction != null)
+            //{
+            //    await _transaction.RollbackAsync();
+            //    await _transaction.DisposeAsync();
+            //    _transaction = null;
+            //}
+        }
+
+        // Dispose pattern to clean up resources
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+                _disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ src/Infrastructure/Shoppe.Persistence/ServiceRegistrations.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Shoppe.Application.Abstractions.Repositories;
+using Shoppe.Application.Abstractions.UoW;
+using Shoppe.Persistence.Concretes.Repositories;
+using Shoppe.Persistence.Concretes.UoW;
+using Shoppe.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoppe.Persistence
+{
+    public static class ServiceRegistrations
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ShoppeDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("Default")));
+            //services.AddScoped<IDbContextTransaction, IDbContextTransaction>();
+            services.AddScoped<IProductReadRepository, ProductReadRepository>();
+            services.AddScoped<IProductWriteRepository, ProductWriteRepository>();
+
+            // Register UnitOfWork
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            return services;
+        }
+    }
+}
