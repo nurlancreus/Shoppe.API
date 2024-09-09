9ad19219533c780896bdface26e061060ae6de25
nurlancreus <nurlancreus007@gmail.com>
2024-09-09 17:59:23 +0400

    modifying program.cs

--- src/Presentation/Shoppe.API/Program.cs
+++ src/Presentation/Shoppe.API/Program.cs
@@ -1,4 +1,13 @@
 
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
+using Serilog;
+using Shoppe.API.Configurations;
+using Shoppe.API.Middlewares;
+using Shoppe.Persistence;
+using System.Threading.RateLimiting;
+
 namespace Shoppe.API
 {
     public class Program
@@ -8,13 +17,44 @@ namespace Shoppe.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddHttpContextAccessor()
+                .AddPersistenceServices(builder.Configuration);
+
+            builder.ConfigureLogging();
 
             builder.Services.AddControllers();
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            //Enable versioning in Web API controllers
+            builder.Services.AddApiVersioning(config =>
+            {
+                config.ApiVersionReader = new UrlSegmentApiVersionReader(); //Reads version number from request url at "apiVersion" constraint
+
+                //config.ApiVersionReader = new QueryStringApiVersionReader(); //Reads version number from request query string called "api-version". Eg: api-version=1.0
+
+                //config.ApiVersionReader = new HeaderApiVersionReader("api-version"); //Reads version number from request header called "api-version". Eg: api-version: 1.0
+
+                config.DefaultApiVersion = new ApiVersion(1, 0);
+                config.AssumeDefaultVersionWhenUnspecified = true;
+            });
+
+            // Implement built-in rate limiter
+            builder.Services.AddRateLimiter(options => options
+            .AddFixedWindowLimiter(policyName: "fixed", limiterOptions =>
+            {
+                limiterOptions.PermitLimit = 100;
+                limiterOptions.Window = TimeSpan.FromMinutes(1);
+                limiterOptions.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+                limiterOptions.QueueLimit = 5;
+            }));
+
             var app = builder.Build();
+
+            //Use rate limiter
+            app.UseRateLimiter();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -23,10 +63,21 @@ namespace Shoppe.API
                 app.UseSwaggerUI();
             }
 
+            // should put above everything you want to log (only logs the things coming after itself)
+            app.UseSerilogRequestLogging();
+
+            app.UseStaticFiles();
+
+            // for http logging
+            app.UseHttpLogging();
+
             app.UseHttpsRedirection();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
+            // should put after UseAuth* middlewares
+            app.UseMyLoggingMiddleware();
 
             app.MapControllers();
 
