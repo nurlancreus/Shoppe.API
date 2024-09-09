7e0aa1bae2899406d692366eb15fc5b074cf5ff8
nurlancreus <nurlancreus007@gmail.com>
2024-09-09 17:58:59 +0400

    configuring db context

---
+++ src/Infrastructure/Shoppe.Persistence/Context/ShoppeDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shoppe.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoppe.Persistence.Context
+{
+    public class ShoppeDbContext : DbContext
+    {
+        public ShoppeDbContext()
+        {
+            
+        }
+        public ShoppeDbContext(DbContextOptions<ShoppeDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
