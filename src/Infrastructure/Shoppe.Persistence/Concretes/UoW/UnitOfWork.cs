4b322f61a84328132c345e4ed3114721a63396a1
nurlancreus <nurlancreus007@gmail.com>
2024-09-09 18:09:55 +0400

    fixing UnitOfWork implementation

--- src/Infrastructure/Shoppe.Persistence/Concretes/UoW/UnitOfWork.cs
+++ src/Infrastructure/Shoppe.Persistence/Concretes/UoW/UnitOfWork.cs
@@ -1,31 +1,31 @@
-﻿using Microsoft.EntityFrameworkCore.Storage;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 using Shoppe.Application.Abstractions.Repositories;
 using Shoppe.Application.Abstractions.UoW;
 using Shoppe.Persistence.Context;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Shoppe.Persistence.Concretes.UoW
 {
-    public class UnitOfWork : IUnitOfWork
+    public class UnitOfWork : IUnitOfWork, IDisposable
     {
         private readonly ShoppeDbContext _context;
-        //private IDbContextTransaction _transaction;
+        private IDbContextTransaction? _transaction;
         private bool _disposed = false;
 
         // Entity-specific repositories
         public IProductReadRepository ProductReadRepository { get; private set; }
         public IProductWriteRepository ProductWriteRepository { get; private set; }
 
-        public UnitOfWork(ShoppeDbContext context, IProductReadRepository productReadRepository, IProductWriteRepository productWriteRepository)
+        public UnitOfWork(
+            ShoppeDbContext context,
+            IProductReadRepository productReadRepository,
+            IProductWriteRepository productWriteRepository)
         {
-            _context = context;
-            //_transaction = transaction;
-            ProductReadRepository = productReadRepository;
-            ProductWriteRepository = productWriteRepository;
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            ProductReadRepository = productReadRepository ?? throw new ArgumentNullException(nameof(productReadRepository));
+            ProductWriteRepository = productWriteRepository ?? throw new ArgumentNullException(nameof(productWriteRepository));
         }
 
         public async Task<int> SaveAsync()
@@ -35,19 +35,21 @@ namespace Shoppe.Persistence.Concretes.UoW
 
         public async Task BeginTransactionAsync()
         {
-
-            using var transaction = await _context.Database.BeginTransactionAsync();
+            if (_transaction == null)
+            {
+                _transaction = await _context.Database.BeginTransactionAsync();
+            }
         }
 
         public async Task CommitTransactionAsync()
         {
             try
             {
-                await _context.SaveChangesAsync();
-                //if (_transaction != null)
-                //{
-                //    await _transaction.CommitAsync();
-                //}
+                await SaveAsync();
+                if (_transaction != null)
+                {
+                    await _transaction.CommitAsync();
+                }
             }
             catch
             {
@@ -56,22 +58,22 @@ namespace Shoppe.Persistence.Concretes.UoW
             }
             finally
             {
-                //if (_transaction != null)
-                //{
-                //    await _transaction.DisposeAsync();
-                //    _transaction = null;
-                //}
+                if (_transaction != null)
+                {
+                    await _transaction.DisposeAsync();
+                    _transaction = null;
+                }
             }
         }
 
         public async Task RollbackTransactionAsync()
         {
-            //if (_transaction != null)
-            //{
-            //    await _transaction.RollbackAsync();
-            //    await _transaction.DisposeAsync();
-            //    _transaction = null;
-            //}
+            if (_transaction != null)
+            {
+                await _transaction.RollbackAsync();
+                await _transaction.DisposeAsync();
+                _transaction = null;
+            }
         }
 
         // Dispose pattern to clean up resources
