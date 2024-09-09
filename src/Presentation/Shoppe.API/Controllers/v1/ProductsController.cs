821199b48217f8ab8115b0e4f896c5e2b9df96ef
nurlancreus <nurlancreus007@gmail.com>
2024-09-09 17:58:20 +0400

    add controllers with versioning

---
+++ src/Presentation/Shoppe.API/Controllers/v1/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shoppe.Application.Abstractions.UoW;
+
+namespace Shoppe.API.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    public class ProductsController : ApplicationControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var products = await _unitOfWork.ProductReadRepository.GetAllAsync();
+
+            return Ok(products);
+        }
+    }
+}
