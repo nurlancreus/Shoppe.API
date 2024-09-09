927488790657586da4ff399b361cbecfd37d3932
nurlancreus <nurlancreus007@gmail.com>
2024-09-09 18:01:26 +0400

    implement UoW pattern

---
+++ src/Core/Shoppe.Application/Abstractions/UoW/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shoppe.Application.Abstractions.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoppe.Application.Abstractions.UoW
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IProductReadRepository ProductReadRepository { get; }
+        IProductWriteRepository ProductWriteRepository { get; }
+        Task<int> SaveAsync();
+        Task BeginTransactionAsync();
+        Task CommitTransactionAsync();
+        Task RollbackTransactionAsync();
+    }
+}
