d210d126fbf13b04e9063b49179d54d425f565cd
nurlancreus <nurlancreus007@gmail.com>
2024-09-10 15:26:09 +0400

    adding new DbSets and configuration classes

--- src/Infrastructure/Shoppe.Persistence/Context/ShoppeDbContext.cs
+++ src/Infrastructure/Shoppe.Persistence/Context/ShoppeDbContext.cs
@@ -1,24 +1,37 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Shoppe.Domain.Entities;
+using Shoppe.Domain.Entities.Files;
+using Shoppe.Domain.Entities.Identity;
+using Shoppe.Persistence.Configurations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Shoppe.Persistence.Context
 {
-    public class ShoppeDbContext : DbContext
+    public class ShoppeDbContext(DbContextOptions<ShoppeDbContext> dbContextOptions) : IdentityDbContext<ApplicationUser, ApplicationRole, string>(dbContextOptions)
     {
-        public ShoppeDbContext()
+        protected override void OnModelCreating(ModelBuilder builder)
         {
-            
-        }
-        public ShoppeDbContext(DbContextOptions<ShoppeDbContext> dbContextOptions) : base(dbContextOptions)
-        {
-            
-        }
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetAssembly(typeof(ProductConfiguration))!);
 
+            base.OnModelCreating(builder);
+        }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ProductDetails> ProductDetails { get; set; }
+        public DbSet<ProductDimension> ProductDimensions { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<SocialLink> SocialLinks { get; set; }
+        public DbSet<ApplicationFile> ApplicationFiles { get; set; }
+        public DbSet<ImageFile> ImageFiles { get; set; } 
+        public DbSet<ProductImageFile> ProductImageFiles { get; set; }
+        public DbSet<BlogImageFile> BlogImageFiles { get; set; }
     }
 }
--- src/Infrastructure/Shoppe.Persistence/ServiceRegistrations.cs
+++ src/Infrastructure/Shoppe.Persistence/ServiceRegistrations.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Shoppe.Application.Abstractions.Repositories;
 using Shoppe.Application.Abstractions.UoW;
+using Shoppe.Domain.Entities.Identity;
 using Shoppe.Persistence.Concretes.Repositories;
 using Shoppe.Persistence.Concretes.UoW;
 using Shoppe.Persistence.Context;
@@ -12,6 +14,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace Shoppe.Persistence
 {
