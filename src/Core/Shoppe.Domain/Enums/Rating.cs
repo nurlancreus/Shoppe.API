f861ba9c91d0a12c3ae7f4e1cecbba1a1e802b9f
nurlancreus <nurlancreus007@gmail.com>
2024-09-10 15:25:11 +0400

    adding enums

---
+++ src/Core/Shoppe.Domain/Enums/Rating.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoppe.Domain.Enums
+{
+    public enum Rating
+    {
+        Poor = 1,
+        Fair,
+        Good,
+        VeryGood,
+        Excellent
+    }
+}
---
+++ src/Core/Shoppe.Domain/Enums/SocialPlatform.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoppe.Domain.Enums
+{
+    public enum SocialPlatform
+    {
+        Facebook,
+        X,
+        Instagram,
+        Youtube
+    }
+}
