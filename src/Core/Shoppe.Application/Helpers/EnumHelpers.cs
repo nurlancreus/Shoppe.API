1e610dc689e80998b3a61c661de06e5584fdacde
nurlancreus <nurlancreus007@gmail.com>
2024-09-10 18:38:14 +0400

    adding file and enum helpers

---
+++ src/Core/Shoppe.Application/Helpers/EnumHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoppe.Application.Helpers
+{
+    public static class EnumHelpers
+    {
+        public static bool TryParseEnum<T>(string value, out T result, bool caseInsentitive = true) where T : struct, Enum
+        {
+            // Use Enum.TryParse with ignoreCase = true for case-insensitive parsing
+            return Enum.TryParse(value, ignoreCase: caseInsentitive, result: out result);
+        }
+    }
+}
