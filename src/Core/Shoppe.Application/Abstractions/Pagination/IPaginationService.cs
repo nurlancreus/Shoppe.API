09951b1360f0badfd4a07efce2e657ba2c60e33f
nurlancreus <nurlancreus007@gmail.com>
2024-09-11 20:48:03 +0400

    adding Pagination Service and info

---
+++ src/Core/Shoppe.Application/Abstractions/Pagination/IPaginationService.cs
@@ -0,0 +1,16 @@
+﻿using Shoppe.Application.DTOs.Pagination;
+using Shoppe.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Shoppe.Application.Abstractions.Pagination
+{
+    public interface IPaginationService
+    {
+        Task<PaginatedQueryDTO<T>> ConfigurePaginationAsync<T>(int page, int pageSize, IQueryable<T> entities) where T : BaseEntity;
+    }
+}
---
+++ src/Core/Shoppe.Application/Abstractions/Pagination/PaginationInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoppe.Application.Abstractions.Pagination
+{
+    public interface IPaginationInfo
+    {
+        int Page { get; set; }
+        int PageSize { get; set; }
+        int TotalPages { get; set; }
+        int TotalItems { get; set; }
+    }
+}
