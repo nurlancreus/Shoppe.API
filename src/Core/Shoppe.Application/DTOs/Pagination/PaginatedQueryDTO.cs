eefab54b110bb434c5b06e981aecba6843eae3bb
nurlancreus <nurlancreus007@gmail.com>
2024-09-11 20:48:15 +0400

    adding paginated query DTO

---
+++ src/Core/Shoppe.Application/DTOs/Pagination/PaginatedQueryDTO.cs
@@ -0,0 +1,28 @@
+﻿using Shoppe.Application.Abstractions.Pagination;
+using Shoppe.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoppe.Application.DTOs.Pagination
+{
+    public class PaginatedQueryDTO<T> : IPaginationInfo where T : BaseEntity
+    {
+        public IQueryable<T> PaginatedQuery { get; set; } = null!;
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }
+        public int TotalItems { get; set; }
+
+        public void Deconstruct(out int totalItems, out int pageSize, out int page, out int totalPages, out IQueryable<T> paginatedQuery)
+        {
+            totalItems = TotalItems;
+            pageSize = PageSize;
+            page = Page;
+            totalPages = TotalPages;
+            paginatedQuery = PaginatedQuery;
+        }
+    }
+}
