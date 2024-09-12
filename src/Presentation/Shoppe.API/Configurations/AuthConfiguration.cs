45a6a86c3824782f63816ff37ea3981c289979e8
nurlancreus <nurlancreus007@gmail.com>
2024-09-12 19:31:16 +0400

    implement new configurations

---
+++ src/Presentation/Shoppe.API/Configurations/AuthConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+
+namespace Shoppe.API.Configurations
+{
+    public static class AuthConfiguration
+    {
+        public static void ConfigureAuth(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+             .AddJwtBearer(options =>
+             {
+                 options.TokenValidationParameters = new TokenValidationParameters()
+                 {
+                     ValidateAudience = true,
+                     ValidAudience = builder.Configuration["Token:Access:Audience"],
+                     ValidateIssuer = true,
+                     ValidIssuer = builder.Configuration["Token:Access:Issuer"],
+                     ValidateLifetime = true,
+                     ValidateIssuerSigningKey = true,
+                     IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(builder.Configuration["Token:Access:SecurityKey"]!)),
+
+                     LifetimeValidator = (notBefore, expires, securityToken, validationParameters) => expires != null && expires > DateTime.UtcNow,
+
+                     NameClaimType = ClaimTypes.Name, // The value corresponding to the Name claim in the JWT can be obtained from the User.Identity.Name property.
+                     RoleClaimType = ClaimTypes.Role
+                 };
+             });
+        }
+    }
+}
--- src/Presentation/Shoppe.API/Configurations/OptionsConfiguration.cs
+++ src/Presentation/Shoppe.API/Configurations/OptionsConfiguration.cs
@@ -1,13 +1,15 @@
-﻿using Shoppe.Application.Options.Storage;
+﻿using Microsoft.AspNetCore.Identity;
+using Shoppe.Application.Options.Storage;
 using System.Configuration;
 
 namespace Shoppe.API.Configurations
 {
-    public static class OptionsConfigurations
+    public static class OptionsConfiguration
     {
         public static void ConfigureOptions (this WebApplicationBuilder builder)
         {
             builder.Services.Configure<StorageOptions>(builder.Configuration.GetSection("Storage"));
+            builder.Services.Configure<TokenOptions>(builder.Configuration.GetSection("Token"));
         }
     }
 }
