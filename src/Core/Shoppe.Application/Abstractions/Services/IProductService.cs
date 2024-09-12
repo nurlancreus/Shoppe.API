a673149312e1533993acc150f8e6c5a28b13a4fa
nurlancreus <nurlancreus007@gmail.com>
2024-09-12 19:34:03 +0400

    implement productservice

--- src/Core/Shoppe.Application/Abstractions/Services/IProductService.cs
+++ src/Core/Shoppe.Application/Abstractions/Services/IProductService.cs
@@ -10,5 +10,9 @@ namespace Shoppe.Application.Abstractions.Services
     public interface IProductService
     {
         Task CreateProductAsync(CreateProductDTO createProductDTO, CancellationToken cancellationToken);
+        Task UpdateProductAsync(UpdateProductDTO updateProductDTO, CancellationToken cancellationToken);
+        Task DeleteProductAsync(string id, CancellationToken cancellationToken);
+        Task<GetProductDTO> GetProductAsync(string id, CancellationToken cancellationToken);
+        Task<GetAllProductsDTO> GetAllProductsAsync(int page, int size, CancellationToken cancellationToken);
     }
 }
