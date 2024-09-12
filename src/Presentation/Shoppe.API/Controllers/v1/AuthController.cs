8ff8e6b33a6772d15af3992cc9a8557d3d49436a
nurlancreus <nurlancreus007@gmail.com>
2024-09-12 19:31:31 +0400

    implement auth and products controllers

---
+++ src/Presentation/Shoppe.API/Controllers/v1/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shoppe.Application.Features.Command.Auth.Login;
+using Shoppe.Application.Features.Command.Auth.RefreshLogin;
+using Shoppe.Application.Features.Command.Auth.Register;
+
+namespace Shoppe.API.Controllers.v1
+{
+    //[ApiVersion("1.0")]
+    public class AuthController : ApplicationControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AuthController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Register(RegisterCommandRequest registerCommandRequest)
+        {
+            var response = await _mediator.Send(registerCommandRequest);
+
+            return Ok(response);
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Login(LoginCommandRequest loginCommandRequest)
+        {
+            var response = await _mediator.Send(loginCommandRequest);
+
+            return Ok(response);
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> RefreshLogin(RefreshLoginCommandRequest refreshLoginCommandRequest)
+        {
+            var response = await _mediator.Send(refreshLoginCommandRequest);
+
+            return Ok(response);
+        }
+    }
+}
--- src/Presentation/Shoppe.API/Controllers/v1/ProductsController.cs
+++ src/Presentation/Shoppe.API/Controllers/v1/ProductsController.cs
@@ -4,6 +4,10 @@ using Microsoft.AspNetCore.Mvc;
 using Shoppe.Application.Abstractions.Repositories;
 using Shoppe.Application.Abstractions.UoW;
 using Shoppe.Application.Features.Command.Product.CreateProduct;
+using Shoppe.Application.Features.Command.Product.DeleteProduct;
+using Shoppe.Application.Features.Command.Product.UpdateProduct;
+using Shoppe.Application.Features.Query.Product.GetAllProducts;
+using Shoppe.Application.Features.Query.Product.GetProductById;
 using Shoppe.Domain.Entities;
 
 namespace Shoppe.API.Controllers.v1
@@ -19,12 +23,55 @@ namespace Shoppe.API.Controllers.v1
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create(CreateProductCommandRequest createProductCommandRequest)
+        public async Task<IActionResult> Create([FromForm] CreateProductCommandRequest createProductCommandRequest)
         {
             var response = await _mediator.Send(createProductCommandRequest);
 
             return Ok(response);
         }
 
+        [HttpGet]
+        public async Task<IActionResult> GetAll([FromQuery] GetAllProductsQueryRequest getAllProductsQueryRequest)
+        {
+            var response = await _mediator.Send(getAllProductsQueryRequest);
+
+            return Ok(response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            var getProductByIdQueryRequest = new GetProductByIdQueryRequest()
+            {
+                Id = id
+            };
+
+            var response = await _mediator.Send(getProductByIdQueryRequest);
+
+            return Ok(response);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update([FromRoute] string id, [FromForm] UpdateProductCommandRequest updateProductCommandRequest)
+        {
+            updateProductCommandRequest.Id = id;
+
+            var response = await _mediator.Send(updateProductCommandRequest);
+
+            return Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var deleteProductCommandRequest = new DeleteProductCommandRequest()
+            {
+                Id = id
+            };
+
+            var response = await _mediator.Send(deleteProductCommandRequest);
+
+            return Ok(response);
+        }
     }
 }
