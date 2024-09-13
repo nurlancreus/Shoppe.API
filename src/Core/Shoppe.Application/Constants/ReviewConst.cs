f2acc1bf382eaea9008526855befe2cfd5c2abae
nurlancreus <nurlancreus007@gmail.com>
2024-09-13 16:33:43 +0400

    add reviewconst.

---
+++ src/Core/Shoppe.Application/Constants/ReviewConst.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoppe.Application.Constants
+{
+    public class ReviewConst
+    {
+        public const int MaxBodyLength = 300;
+        public const int MaxFirstNameLength = 50;
+        public const int MaxLastNameLength = 50;
+    }
+}
