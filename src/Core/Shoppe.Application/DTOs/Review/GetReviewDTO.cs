8e6ef6813a71d135df18f9fcea64714ebfec8907
nurlancreus <nurlancreus007@gmail.com>
2024-09-13 16:33:52 +0400

    add review DTOs

---
+++ src/Core/Shoppe.Application/DTOs/Review/GetReviewDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoppe.Application.DTOs.Review
+{
+    public class GetReviewDTO
+    {
+        public string Id { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string? Body { get; set; }
+        public int Rating { get; set; } 
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ src/Core/Shoppe.Application/DTOs/Review/UpdateReviewDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoppe.Application.DTOs.Review
+{
+    public class UpdateReviewDTO
+    {
+        public string Id { get; set; } = null!;
+        public string? Body { get; set; }
+        public int? Rating { get; set; }
+    }
+}
