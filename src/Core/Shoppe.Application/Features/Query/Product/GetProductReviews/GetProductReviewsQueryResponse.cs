07038306c7890b2982956ae38ee1af378e8c2548
nurlancreus <nurlancreus007@gmail.com>
2024-09-13 16:34:34 +0400

    add ReviewPerProduct query

---
+++ src/Core/Shoppe.Application/Features/Query/Product/GetProductReviews/GetProductReviewsQueryResponse.cs
@@ -0,0 +1,14 @@
+﻿using Shoppe.Application.DTOs.Review;
+using Shoppe.Application.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoppe.Application.Features.Query.Product.GetProductReviews
+{
+    public class GetProductReviewsQueryResponse : AppResponseWithData<List<GetReviewDTO>>
+    {
+    }
+}
