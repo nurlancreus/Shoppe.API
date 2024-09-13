a488b71280cf5fe8f2bad71d56714846905da721
nurlancreus <nurlancreus007@gmail.com>
2024-09-13 16:29:36 +0400

    implement new ReviewRepos

---
+++ src/Infrastructure/Shoppe.Persistence/Concretes/Repositories/ReviewRepos/ReviewWriteRepository.cs
@@ -0,0 +1,18 @@
+﻿using Shoppe.Application.Abstractions.Repositories.ReviewRepos;
+using Shoppe.Domain.Entities;
+using Shoppe.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoppe.Persistence.Concretes.Repositories.ReviewRepos
+{
+    public class ReviewWriteRepository : WriteRepository<Review>, IReviewWriteRepository
+    {
+        public ReviewWriteRepository(ShoppeDbContext context) : base(context)
+        {
+        }
+    }
+}
