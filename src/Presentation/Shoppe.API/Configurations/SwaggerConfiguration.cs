709a229a3aac1a4e2da33723b0088c7f9d0b2918
nurlancreus <nurlancreus007@gmail.com>
2024-09-13 16:34:44 +0400

    Modify Swagger gen

--- src/Presentation/Shoppe.API/Configurations/SwaggerConfiguration.cs
+++ src/Presentation/Shoppe.API/Configurations/SwaggerConfiguration.cs
@@ -1,8 +1,10 @@
-﻿namespace Shoppe.API.Configurations
+﻿using Microsoft.OpenApi.Models;
+
+namespace Shoppe.API.Configurations
 {
     public static class SwaggerConfiguration
     {
-        public static void ConfigureSwaggerGen (this WebApplicationBuilder builder)
+        public static void ConfigureSwaggerGen(this WebApplicationBuilder builder)
         {
             builder.Services.AddSwaggerGen(options =>
             {
@@ -11,6 +13,28 @@
                 options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo() { Title = "Cities Web API", Version = "1.0" });
 
                 options.SwaggerDoc("v2", new Microsoft.OpenApi.Models.OpenApiInfo() { Title = "Cities Web API", Version = "2.0" });
+
+                // Add JWT bearer authorization to Swagger
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter token with 'Bearer' prefix, e.g., 'Bearer your-token'",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement {
+            {
+                new OpenApiSecurityScheme {
+                    Reference = new OpenApiReference {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                new string[] { }
+            }
+        });
 
             }); //generates OpenAPI specification
         }
