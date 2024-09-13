ab5310f074cee5cab9597942bcca962f89afed27
nurlancreus <nurlancreus007@gmail.com>
2024-09-13 16:33:26 +0400

    add new invalidIdException

---
+++ src/Core/Shoppe.Domain/Exceptions/InvalidIdException.cs
@@ -0,0 +1,29 @@
+﻿using Shoppe.Domain.Exceptions.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoppe.Domain.Exceptions
+{
+    public class InvalidIdException : BaseException
+    {
+        public override string Title => nameof(InvalidIdException);
+        public InvalidIdException()
+            : base("Invalid Id.", HttpStatusCode.BadRequest)
+        {
+        }
+
+        public InvalidIdException(string message)
+            : base(message, HttpStatusCode.BadRequest)
+        {
+        }
+
+        public InvalidIdException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
