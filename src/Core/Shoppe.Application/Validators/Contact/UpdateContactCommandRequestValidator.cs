04e1d48bad9a082dd2c88728f9f429ad86718ded
nurlancreus <nurlancreus007@gmail.com>
2024-09-14 17:51:17 +0400

    Add Contact Command validators

---
+++ src/Core/Shoppe.Application/Validators/Contact/UpdateContactCommandRequestValidator.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using Shoppe.Application.Constants;
+using Shoppe.Application.Features.Command.Contact.UpdateContact;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoppe.Application.Validators.Contact
+{
+    public class UpdateContactCommandRequestValidator : AbstractValidator<UpdateContactCommandRequest>
+    {
+        public UpdateContactCommandRequestValidator()
+        {
+            RuleFor(x => x.FirstName)
+                .NotEmpty().WithMessage("First Name is required.")
+                .MaximumLength(ContactConst.MaxFirstNameLength).WithMessage($"First Name cannot exceed {ContactConst.MaxFirstNameLength} characters.");
+
+            RuleFor(x => x.LastName)
+                .NotEmpty().WithMessage("Last Name is required.")
+                .MaximumLength(ContactConst.MaxLastNameLength).WithMessage($"Last Name cannot exceed {ContactConst.MaxLastNameLength} characters.");
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("Invalid email format.");
+
+            RuleFor(x => x.Subject)
+                .NotEmpty().WithMessage("Subject is required.")
+                .MaximumLength(ContactConst.MaxSubjectLength).WithMessage($"Subject cannot exceed {ContactConst.MaxSubjectLength} characters.");
+
+            RuleFor(x => x.Message)
+                .NotEmpty().WithMessage("Message is required.")
+                .MaximumLength(ContactConst.MaxMessageLength).WithMessage($"Message cannot exceed {ContactConst.MaxMessageLength} characters.");
+        }
+    }
+}
