1c30e20d2700bc89aaa80d7a36e245360e0d2297
nurlancreus <nurlancreus007@gmail.com>
2024-09-14 17:49:22 +0400

    implement Contact repos

---
+++ src/Infrastructure/Shoppe.Persistence/Concretes/Repositories/ContactRepos/ContactReadRepository.cs
@@ -0,0 +1,18 @@
+﻿using Shoppe.Application.Abstractions.Repositories.ContactRepos;
+using Shoppe.Domain.Entities;
+using Shoppe.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoppe.Persistence.Concretes.Repositories.ContactRepos
+{
+    public class ContactReadRepository : ReadRepository<Contact>, IContactReadRepository
+    {
+        public ContactReadRepository(ShoppeDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ src/Infrastructure/Shoppe.Persistence/Concretes/Repositories/ContactRepos/ContactWriteRepository.cs
@@ -0,0 +1,18 @@
+﻿using Shoppe.Application.Abstractions.Repositories.ContactRepos;
+using Shoppe.Domain.Entities;
+using Shoppe.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoppe.Persistence.Concretes.Repositories.ContactRepos
+{
+    public class ContactWriteRepository : WriteRepository<Contact>, IContactWriteRepository
+    {
+        public ContactWriteRepository(ShoppeDbContext context) : base(context)
+        {
+        }
+    }
+}
