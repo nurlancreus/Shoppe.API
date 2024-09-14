e766cd79683b8fe30c5f3fad11a2ed103870deb0
nurlancreus <nurlancreus007@gmail.com>
2024-09-14 17:49:52 +0400

    Add Contact DTOs

---
+++ src/Core/Shoppe.Application/DTOs/Contact/UpdateContactDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoppe.Application.DTOs.Contact
+{
+    public class UpdateContactDTO
+    {
+        public string Id { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Subject { get; set; } = null!;
+        public string Message { get; set; } = null!;
+    }
+}
