e603ffafab21fd010ebccabd82d3d1bac964a0fc
nurlancreus <nurlancreus007@gmail.com>
2024-09-14 17:51:44 +0400

    add AddNotSucceedException

---
+++ src/Core/Shoppe.Domain/Exceptions/AddNotSucceedException.cs
@@ -0,0 +1,29 @@
+﻿using Shoppe.Domain.Exceptions.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoppe.Domain.Exceptions
+{
+    public class AddNotSucceedException : BaseException
+    {
+        public override string Title => nameof(AddNotSucceedException);
+        public AddNotSucceedException()
+            : base("The entity could not be added.", HttpStatusCode.BadRequest)
+        {
+        }
+
+        public AddNotSucceedException(string message)
+            : base(message, HttpStatusCode.BadRequest)
+        {
+        }
+
+        public AddNotSucceedException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
