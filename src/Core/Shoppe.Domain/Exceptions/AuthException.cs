0251a2850b0ec4024fb0179202c3039fdc298fbc
nurlancreus <nurlancreus007@gmail.com>
2024-10-12 16:09:51 +0400

    add new exceptions

--- src/Core/Shoppe.Domain/Exceptions/AuthException.cs
+++ src/Core/Shoppe.Domain/Exceptions/AuthException.cs
@@ -1,39 +1,52 @@
 ﻿using Shoppe.Domain.Exceptions.Base;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Shoppe.Domain.Exceptions
 {
     public enum AuthErrorType
     {
         Register,
-        Login
+        Login,
+        Authorize
     }
+
     public class AuthException : BaseException
     {
         public override string Title => nameof(AuthException);
         public AuthErrorType ErrorType { get; }
+
+        // Modify the constructor to set status code based on error type
         public AuthException(AuthErrorType errorType)
-             : base(GenerateMessage(errorType), HttpStatusCode.BadRequest)
+            : base(GenerateMessage(errorType), DetermineStatusCode(errorType))
         {
             ErrorType = errorType;
         }
 
-        public AuthException(string? message, Exception? innerException) : base(message, innerException)
+        public AuthException(string? message, Exception? innerException)
+            : base(message, innerException)
         {
         }
 
+        // Generate message based on error type
         private static string GenerateMessage(AuthErrorType errorType)
         {
             return errorType switch
             {
                 AuthErrorType.Register => "Cannot register user. Wrong credentials",
                 AuthErrorType.Login => "Cannot login user. Wrong credentials",
+                AuthErrorType.Authorize => "User is not authorized",
                 _ => "Wrong credentials"
+            };
+        }
+
+        // Determine status code based on error type
+        private static HttpStatusCode DetermineStatusCode(AuthErrorType errorType)
+        {
+            return errorType switch
+            {
+                AuthErrorType.Authorize => HttpStatusCode.Unauthorized, // 401 for unauthorized
+                _ => HttpStatusCode.BadRequest // 400 for register or login errors
             };
         }
     }
---
+++ src/Core/Shoppe.Domain/Exceptions/InvalidFilterException.cs
@@ -0,0 +1,53 @@
+﻿using Shoppe.Domain.Exceptions.Base;
+using System;
+using System.Net;
+
+namespace Shoppe.Domain.Exceptions
+{
+    public enum FilterErrorType
+    {
+        InvalidSortValue,
+        InvalidFilterValue
+    }
+
+    public class InvalidFilterException : BaseException
+    {
+        public override string Title => nameof(InvalidFilterException);
+        public FilterErrorType ErrorType { get; }
+        public string? FilterField { get; }
+
+        public InvalidFilterException(FilterErrorType errorType, string filterField)
+            : base(GenerateMessage(errorType, filterField), HttpStatusCode.BadRequest)
+        {
+            ErrorType = errorType;
+            FilterField = filterField;
+        }
+
+        public InvalidFilterException(string message)
+            : base(message, HttpStatusCode.BadRequest)
+        {
+        }
+
+        public InvalidFilterException(string? message, Exception? innerException)
+            : base(message, innerException)
+        {
+        }
+
+        public InvalidFilterException(string customMessage, FilterErrorType errorType, string filterField)
+            : base(GenerateMessage(errorType, filterField, customMessage), HttpStatusCode.BadRequest)
+        {
+            ErrorType = errorType;
+            FilterField = filterField;
+        }
+
+        private static string GenerateMessage(FilterErrorType errorType, string filterField, string? customMessage = null)
+        {
+            return customMessage ?? errorType switch
+            {
+                FilterErrorType.InvalidSortValue => $"The sorting option '{filterField}' is invalid. Please choose a valid sorting option.",
+                FilterErrorType.InvalidFilterValue => $"The filter value '{filterField}' is invalid. Please ensure it meets the required criteria.",
+                _ => "An invalid filter parameter was provided. Please check the filter parameters and try again."
+            };
+        }
+    }
+}
