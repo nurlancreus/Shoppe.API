70e14929ceb27a3593348aaa36314aa69bc3a736
nurlancreus <nurlancreus007@gmail.com>
2024-10-20 19:01:56 +0400

    implementing new users and roles controlles

---
+++ src/Presentation/Shoppe.API/Controllers/v1/RolesController.cs
@@ -0,0 +1,89 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Shoppe.Application.Features.Command.Role.AssignUsers;
+using Shoppe.Application.Features.Command.Role.Create;
+using Shoppe.Application.Features.Command.Role.Delete;
+using Shoppe.Application.Features.Command.Role.Update;
+using Shoppe.Application.Features.Query.Role.Get;
+using Shoppe.Application.Features.Query.Role.GetAll;
+using Shoppe.Application.Features.Query.Role.GetUsers;
+
+namespace Shoppe.API.Controllers.v1
+{
+    public class RolesController : ApplicationControllerBase
+    {
+        private readonly ISender _sender;
+
+        public RolesController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            var request = new GetRoleQueryRequest { RoleId = id };
+
+            var response = await _sender.Send(request);
+
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var request = new GetAllRolesQueryRequest();
+            var response = await _sender.Send(request);
+
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateRoleCommandRequest request)
+        {
+            var response = await _sender.Send(request);
+
+            return Ok(response);
+        }
+
+        [HttpPatch("{id}")]
+        public async Task<IActionResult> Update([FromRoute] string id, [FromBody] UpdateRoleCommandRequest request)
+        {
+            request.RoleId = id;
+
+            var response = await _sender.Send(request);
+
+            return Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var request = new DeleteRoleCommandRequest { RoleId = id };
+
+            var response = await _sender.Send(request);
+
+            return Ok(response);
+        }
+
+        [HttpGet("{id}/users")]
+        public async Task<IActionResult> GetUsers(string id)
+        {
+            var request = new GetUsersByRoleQueryRequest { RoleId = id };
+
+            var response = await _sender.Send(request);
+
+            return Ok(response);
+        }
+
+        [HttpPatch("{id}/users")]
+        public async Task<IActionResult> AssignUser([FromRoute] string id, [FromBody] AssignUsersToRoleCommandRequest request)
+        {
+            request.RoleId = id;
+
+            var response = await _sender.Send(request);
+
+            return Ok(response);
+        }
+    }
+}
---
+++ src/Presentation/Shoppe.API/Controllers/v1/UsersController.cs
@@ -0,0 +1,123 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shoppe.Application.Features.Command.User.AssignRoles;
+using Shoppe.Application.Features.Command.User.ChangeProfilePicture;
+using Shoppe.Application.Features.Command.User.Deactivate;
+using Shoppe.Application.Features.Command.User.Delete;
+using Shoppe.Application.Features.Command.User.RemoveProfilePicture;
+using Shoppe.Application.Features.Command.User.Update;
+using Shoppe.Application.Features.Query.User.Get;
+using Shoppe.Application.Features.Query.User.GetAll;
+using Shoppe.Application.Features.Query.User.GetRoles;
+using Shoppe.Application.Features.Query.User.GetUserPictures;
+
+namespace Shoppe.API.Controllers.v1
+{
+    public class UsersController : ApplicationControllerBase
+    {
+        private readonly ISender _sender;
+
+        public UsersController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll([FromQuery] GetAllUsersQueryRequest request)
+        {
+            var response = await _sender.Send(request);
+
+            return Ok(response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            var request = new GetUserQueryRequest { UserId = id };
+
+            var response = await _sender.Send(request);
+
+            return Ok(response);
+        }
+
+        [HttpGet("{id}/pictures")]
+        public async Task<IActionResult> GetUserPictures(string id)
+        {
+            var request = new GetUserPicturesQueryRequest { UserId = id };
+
+            var response = await _sender.Send(request);
+
+            return Ok(response);
+        }
+
+        [HttpPatch("{id}/pictures/{pictureId}")]
+        public async Task<IActionResult> ChangeProfilePicture(string id, string pictureId)
+        {
+            var request = new ChangeProfilePictureCommandRequest { UserId = id, NewProfilePictureId = pictureId };
+
+            var response = await _sender.Send(request);
+
+            return Ok(response);
+        }
+
+        [HttpDelete("{id}/pictures/{pictureId}")]
+        public async Task<IActionResult> RemoveProfilePicture(string id, string pictureId)
+        {
+            var request = new RemoveProfilePictureCommandRequest { UserId = id, PictureId = pictureId };
+
+            var response = await _sender.Send(request);
+
+            return Ok(response);
+        }
+
+        [HttpPatch("{id}")]
+        public async Task<IActionResult> Update([FromRoute] string id, [FromForm] UpdateUserCommandRequest request)
+        {
+            request.UserId = id;
+
+            var response = await _sender.Send(request);
+
+            return Ok(response);
+        }
+
+        [HttpPatch("{id}/deactivate")]
+        public async Task<IActionResult> Deactivate(string id)
+        {
+            var request = new DeactivateUserCommandRequest { UserId = id };
+
+            var response = await _sender.Send(request);
+
+            return Ok(response);
+        }
+
+        [HttpPatch("{id}/roles")]
+        public async Task<IActionResult> AssignRoles([FromRoute] string id, [FromBody] AssignRolesToUserCommandRequest request)
+        {
+            request.UserId = id;
+
+            var response = await _sender.Send(request);
+
+            return Ok(response);
+        }
+
+        [HttpGet("{id}/roles")]
+        public async Task<IActionResult> GetRoles([FromRoute] string id)
+        {
+            var request = new GetUserRolesQueryRequest { UserId = id };
+            var response = await _sender.Send(request);
+
+            return Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var request = new DeleteUserCommandRequest { UserId = id };
+
+            var response = await _sender.Send(request);
+
+            return Ok(response);
+        }
+    }
+}
