45439203ec2d8901269ceb300ce49f5f5e4cbd10
nurlancreus <nurlancreus007@gmail.com>
2024-10-20 18:59:58 +0400

    implementing user and role constants

---
+++ src/Core/Shoppe.Application/Constants/RoleConst.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoppe.Application.Constants
+{
+    public static class RoleConst
+    {
+        public const int MaxNameLength = 25;
+        public const int MaxDescLength = 150;
+    }
+}
---
+++ src/Core/Shoppe.Application/Constants/UserConst.cs
@@ -0,0 +1,11 @@
+﻿namespace Shoppe.Application.Constants
+{
+    public static class UserConst
+    {
+        public const string ImagesFolder = "user-images";
+        public const int MaxFirstNameLength = 50;
+        public const int MaxLastNameLength = 50;
+        public const int MaxUserNameLength = 20;
+        public const int MaxProfilePictureSizeInMB = 2;
+    }
+}
