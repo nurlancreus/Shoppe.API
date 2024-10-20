5c2655c4e194943e0f7463a14d2fab73860bdbd0
nurlancreus <nurlancreus007@gmail.com>
2024-10-20 19:00:21 +0400

    implementing userProfilePicture repos

---
+++ src/Infrastructure/Shoppe.Persistence/Concretes/Repositories/UserProfilePictureFileRepos/UserProfilePictureFileReadRepository.cs
@@ -0,0 +1,18 @@
+﻿using Shoppe.Application.Abstractions.Repositories.UserProfilePictureRepos;
+using Shoppe.Domain.Entities.Files;
+using Shoppe.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoppe.Persistence.Concretes.Repositories.UserProfilePictureFileRepos
+{
+    public class UserProfilePictureFileReadRepository : ReadRepository<UserProfilePictureFile>, IUserProfilePictureFileReadRepository
+    {
+        public UserProfilePictureFileReadRepository(ShoppeDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ src/Infrastructure/Shoppe.Persistence/Concretes/Repositories/UserProfilePictureFileRepos/UserProfilePictureFileWriteRepository.cs
@@ -0,0 +1,18 @@
+﻿using Shoppe.Application.Abstractions.Repositories.UserProfilePictureFileRepos;
+using Shoppe.Domain.Entities.Files;
+using Shoppe.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoppe.Persistence.Concretes.Repositories.UserProfilePictureFileRepos
+{
+    public class UserProfilePictureFileWriteRepository : WriteRepository<UserProfilePictureFile>, IUserProfilePictureFileWriteRepository
+    {
+        public UserProfilePictureFileWriteRepository(ShoppeDbContext context) : base(context)
+        {
+        }
+    }
+}
