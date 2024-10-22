e2d3f3588b74b02da3d7eca66a0d1530e71e6f0f
nurlancreus <nurlancreus007@gmail.com>
2024-10-22 19:07:16 +0400

    Update Presentation

---
+++ src/Presentation/Shoppe.API/Controllers/v1/AboutController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using Shoppe.Application.Features.Command.About.Update;
+using Shoppe.Application.Features.Query.About.Get; // Assume there's a GetAboutQuery
+using System.Threading.Tasks;
+
+namespace Shoppe.API.Controllers.v1
+{
+    public class AboutController : ApplicationControllerBase
+    {
+        private readonly ISender _sender;
+
+        public AboutController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<GetAboutQueryResponse>> GetAbout(GetAboutQueryRequest request)
+        {
+            
+            var response = await _sender.Send(request);
+
+            return Ok(response);
+        }
+
+        [HttpPatch]
+        public async Task<ActionResult<UpdateAboutCommandResponse>> UpdateAbout([FromBody] UpdateAboutCommandRequest request)
+        {
+            var response = await _sender.Send(request);
+
+            return Ok(response);
+        }
+    }
+}
---
+++ src/Presentation/Shoppe.API/Controllers/v1/SlidersController.cs
@@ -0,0 +1,59 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shoppe.Application.Features.Command.Slider.AddSlide;
+using Shoppe.Application.Features.Command.Slider.Create;
+using Shoppe.Application.Features.Command.Slider.Delete;
+using Shoppe.Application.Features.Command.Slider.Update;
+using Shoppe.Application.Features.Query.Slider.Get;
+
+namespace Shoppe.API.Controllers.v1
+{
+    public class SlidersController : ApplicationControllerBase
+    {
+        private readonly ISender _sender;
+
+        public SlidersController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpGet("{sliderId}")]
+        public async Task<IActionResult> DeleteSlider(string sliderId)
+        {
+            var request = new GetSliderQueryRequest { SliderId = sliderId };
+            var response = await _sender.Send(request);
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromForm] CreateSliderCommandRequest request)
+        {
+            var response = await _sender.Send(request);
+            return Ok(response);
+        }
+
+        [HttpPost("{sliderId}/add-slide")]
+        public async Task<IActionResult> AddSlideToSlider(string sliderId, [FromForm] AddSlideToSliderCommandRequest request)
+        {
+            request.SliderId = sliderId;
+            var response = await _sender.Send(request);
+            return Ok(response);
+        }
+
+        [HttpPatch("update")]
+        public async Task<IActionResult> Update([FromForm] UpdateSliderCommandRequest request)
+        {
+            var response = await _sender.Send(request);
+            return Ok(response);
+        }
+
+        [HttpDelete("{sliderId}")]
+        public async Task<IActionResult> Delete(string sliderId)
+        {
+            var request = new DeleteSliderCommandRequest { SliderId = sliderId };
+            var response = await _sender.Send(request);
+            return Ok(response);
+        }
+    }
+}
